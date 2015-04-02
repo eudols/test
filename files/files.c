6185ce497b8d727b052dc98eb68bdba2d3d91284
Eugenio Dols <eudols@users.noreply.github.com>
2015-04-02 01:11:30 -0700

    Add files folder

---
+++ files/files.c
@@ -0,0 +1,33 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+typedef struct {
+  int x;
+  int y;
+} myint;
+
+int main(int argc, char *argv[]) {
+  FILE *fp;
+  myint mint;
+  myint mint2;
+  mint.x = 3;
+  mint.y = 5;
+  fp = fopen("myfile","w+b");
+  if (NULL == fp) {
+    fprintf(stderr, "An error occurred before\n");
+    return (EXIT_FAILURE);
+  }
+  fwrite(&mint, sizeof(myint), 1, fp);
+  fclose(fp);
+  printf("Test \n");
+
+  fp = fopen("myfile","rb");
+  if (NULL == fp) {
+    fprintf(stderr, "An error occurred now\n");
+    return (EXIT_FAILURE);
+  }
+  fread(&mint2, sizeof(myint), 1, fp);
+  fclose(fp);
+
+  return (EXIT_SUCCESS);
+}
